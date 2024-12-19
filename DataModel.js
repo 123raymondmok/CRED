fd102dfaeb19b06434aeffd8132ac1757e41d513
Raymond Mok <78278340+123raymondmok@users.noreply.github.com>
2024-12-19 14:28:13 -0800

    Add files via upload

---
+++ DataModel.js
@@ -0,0 +1,9 @@
+// models/DataModel.js
+const mongoose = require("mongoose");
+
+const DataSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
